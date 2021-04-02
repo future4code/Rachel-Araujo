d8b62ebd7c6d7d9e1ae9c30b4e715b4cfd89f60c
Rachel-Araujo <rachelaraujo17@gmail.com>
2021-04-01 21:12:27 -0300

    Exercícios aula 11 JavaScript

---
+++ quinzena3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,64 @@
+//Exercícios de interpretação de código
+
+//a.false  b.false  c.true  d. boolean 
+
+//a.undefined  b.null  c.11  d.3  e.(11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13] f.9
+
+
+//Exercícios de escrita de código
+//1.
+//const idade = Number(prompt("Qual a sua idade?"))//a
+//const idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo(a)?"))//b
+
+//console.log(idade, idadeAmigo)
+
+//console.log("Sua idade é maior do que a do seu amigo(a)?", Boolean(idadeAmigo))//c
+
+//const resultado = idade-idadeAmigo
+
+//console.log("A diferença de idade de vocês é de: ", resultado, "anos.")//c
+
+
+//2.
+//const par = Number(prompt("Digite um número par:"))//a
+
+//console.log(par)
+
+//resultado= par % 2
+
+//console.log(Number(resultado))//b
+
+//c, todos aprensentam resto igual a zero.
+//d, se o usuário inserir um número ímpar, o console retorna resto 1.
+
+
+//3.
+
+//const listaDeTarefas = []//a
+// listaDeTarefas.push(prompt("Primeira tarefa do dia:"))//b
+// listaDeTarefas.push(prompt("Segunda tarefa do dia:"))//b
+// listaDeTarefas.push(prompt("Terceira tarefa do dia:"))//b
+
+// console.log(listaDeTarefas)//c
+
+// opcao = Number(prompt("Digite o índice de uma tarefa já realizada: 0, 1 ou 2"))//d
+// console.log(listaDeTarefas[opcao])
+
+// listaDeTarefas.splice([opcao])
+// console.log(listaDeTarefas)
+
+
+4//
+const nome = prompt("Qual o seu nome?")
+const email = prompt("Qual o seu email?")
+
+console.log("O e-mail ", email, "foi cadastrado com sucesso. Seja bem-vinda(o), ", nome,"!")
+
+
+
+
+
+
+
+
+
