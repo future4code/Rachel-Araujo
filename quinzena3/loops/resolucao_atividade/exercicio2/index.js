a33e348a62c00c37acaa4ac908337b55bc3b3d38
Rachel-Araujo <rachelaraujo17@gmail.com>
2021-04-08 22:40:34 -0300

    Atividades Loops

---
+++ quinzena3/loops/resolucao_atividade/exercicio2/index.js
@@ -0,0 +1,44 @@
+const vetorNumeros = [11, 15, 18, 100, 12, 13, 78] // vetorNumeros.length = 7
+let maiorAtual = 0
+
+for (numero of vetorNumeros) { 
+    if(numero > maiorAtual) {
+        maiorAtual = numero
+    }
+}
+
+console.log("O maior número é ", maiorAtual)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ quinzena3/loops/resolucao_atividade/exercicio3/index.js
@@ -0,0 +1,16 @@
+const vetorPalavras = ["Oi", "sumido", "como", "vai?", "Saudades"]
+const vetorLetras = "ABCDEFG"
+let frase1 = ""
+let frase2 = ""
+
+for (palavra of vetorPalavras) {
+    frase1 += palavra + " " 
+    console.log(palavra)
+}
+console.log(frase1)
+
+for (letra of vetorLetras) {
+    frase2 += frase2 + " " 
+    console.log(letra)
+}
+console.log(frase2)
